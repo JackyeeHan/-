c37cedf515f1fa304e8567a6bacadb2439f1d598
Po Han Huang <jacky83522@gmail.com>
2022-06-18 23:18:46 +0800

    finish

--- 讓方塊轉/ViewController.swift
+++ 讓方塊轉/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var counter = 0.0
+    var timer:Timer!
+ 
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var uiView1: UIView!
+    @IBOutlet weak var segment: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        uiView1.transform = CGAffineTransform(rotationAngle: 0)
+        
+        uiView1.layer.cornerRadius = 15
+                
+        timer = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true, block: { (timer) in
+            self.rorate(theview:self.uiView1)
+        })
+        
+        self.view.addSubview(uiView1)
+}
+
+    func rorate(theview:UIView){
+        let angle = counter * Double.pi / 180
+        theview.transform = CGAffineTransform(rotationAngle: CGFloat(angle))
+        
+        if segment.selectedSegmentIndex == 0{
+            counter += Double(slider.value)
+            
+        }else{
+            counter -= Double(slider.value)
+        }
+        
     }
-
-
+    
+    
 }
 
